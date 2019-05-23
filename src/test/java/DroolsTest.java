b3d28c1370f18943b3f7713ac02ee11b65294662
hutianchen <hutianchen@huizhaofang.com>
2019-05-23 18:40:13 +0800

    wiki说明更新

--- src/test/java/DroolsTest.java
+++ src/test/java/DroolsTest.java
@@ -25,7 +25,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.HashMap;
